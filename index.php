eb92045ea8e1de48dd242c5eab7eca4aae04810f
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-02-20 10:59:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <form action="con_create.php" method="POST">
-<input type="text" name="name" placeholder="name"/>
-<input type="text" name="description" placeholder="description"/>
-<input type="text" name="price" placeholder="price"/>
-<input type="text" name="category" placeholder="category"/>
-<input type="submit" name="submit" value="submit"/>
-
+ <tr>
+   <td><input type="text" name="name" placeholder="name"/></td>
+   <td><input type="text" name="description" placeholder="description"/></td>
+   <td><input type="text" name="price" placeholder="price"/></td>
+   <td><input type="text" name="category" placeholder="category"/></td>
+   <td><input type="submit" name="submit" value="submit"/></td>
+  </tr>
 </form>
